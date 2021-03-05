170133c1a53663b3464e64134c347e6ad75d3819
yogi4289 <yogesh.sharma4289@gmail.com>
2021-03-05 16:33:46 +1100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -7,6 +7,6 @@ def lambda_handler(event, context):
   return {
       'statusCode': 200,
       'body': "{}".format(
-          response.text
+          "Lambda function built and deployed by TeamCity & Octopus"
       )
   }
